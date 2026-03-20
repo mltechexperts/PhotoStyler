85a608b6f373e1b695b9835997f0aca1eca7335d
Murali <mlcreativestudios86@gmail.com>
2026-03-20 10:24:50 -0400

    Initial Commit

---
+++ PhotoStyler/PhotoStylerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoStylerApp.swift
+//  PhotoStyler
+//
+//  Created by muralidhar lalagiri on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoStylerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
